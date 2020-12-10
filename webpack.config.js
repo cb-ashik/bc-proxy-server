82c0f1960fa15fb8488e1d0eb72105150251b290
Ashik Nesin <mail@ashiknesin.com>
2020-12-10 12:55:45 +0530

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var path = require('path');
+
+module.exports = {
+    //...
+    
+    devServer: {
+        port: 8888,
+        disableHostCheck: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        proxy: {
+            '/bigcommerce/auth': {
+                target: 'http://app.localcb.in:9090',
+                changeOrigin: true,
+                // pathRewrite: { '/proxy/bigcommerce/auth': '/bigcommerce/auth' },
+                logLevel: 'debug'
+            },
+            '/bigcommerce/load': {
+                target: 'http://app.localcb.in:9090',
+                changeOrigin: true,
+                // pathRewrite: { '/proxy/bigcommerce/load': '/bigcommerce/load' },
+                logLevel: 'debug'
+
+            },
+            '/bigcommerce/uninstall': {
+                target: 'http://app.localcb.in:9090',
+                changeOrigin: true,
+                // pathRewrite: { '/proxy/bigcommerce/uninstall': '/bigcommerce/uninstall' },
+                logLevel: 'debug'
+
+            },
+            '/bigcommerce/finish_install': {
+                target: 'http://app.localcb.in:9090',
+                changeOrigin: true,
+                // pathRewrite: { '/proxy/bigcommerce/uninstall': '/bigcommerce/uninstall' },
+                logLevel: 'debug'
+
+            },
+            '*': {
+                target: 'http://mannar-test.localcb.in:9090',
+                changeOrigin: true,
+                logLevel: 'debug'
+            },
+        
+        }
+    }
+};
